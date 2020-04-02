f15012cf5c5f9f313980464133602cae0348b8ba
toshofan <tramptrump@gmail.com>
2020-04-02 14:45:55 +0300

    first commit

---
+++ drivers/misc/mediatek/imgsensor/src/mt6735m/s5k4h8c_mipi_raw/s5k4h8cmipi_otp.h
@@ -0,0 +1,4 @@
+extern bool otp_wb_update_c(BYTE zone);
+extern bool otp_lenc_update_c(USHORT zone);
+
+
---
+++ drivers/misc/mediatek/imgsensor/src/mt6735m/s5k4h9c_mipi_raw/s5k4h9cmipi_otp.h
@@ -0,0 +1,4 @@
+extern bool otp_wb_update_4h9c(BYTE zone);
+extern bool otp_lenc_update_4h9c(USHORT zone);
+
+
